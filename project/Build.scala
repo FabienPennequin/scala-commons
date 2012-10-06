94a63bbfc12b149e5e58787e50a5e07d37f26b3e
Fabien Pennequin <fabien@pennequin.me>
2012-10-06 23:08:24 +0200

    Created sbt project

---
+++ project/Build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import sbt.Keys._
+
+object ProjectBuild extends Build {
+
+  lazy val root = Project(
+    id = "root",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name          := "scala-commons",
+      organization  := "me.pennequin.fabien",
+      version       := "1.0-SNAPSHOT",
+
+      scalaVersion  := "2.9.1",
+      scalacOptions := Seq("-encoding", "utf8"),
+
+      resolvers     ++= Seq(
+      ),
+
+      libraryDependencies ++= Seq(
+        "org.specs2" %% "specs2" % "1.12.1" % "test"
+      )
+    )
+  )
+
+}
